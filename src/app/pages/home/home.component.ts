da84cc6a2b12388a146b5cc2707c1c820b8cf42d
Italo71 <102165012+italo71@users.noreply.github.com>
2023-05-26 15:55:40 -0300

    vai ler as regras

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AlertService } from './../../shared/service/alert.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CacheService } from 'src/app/shared/service/cache.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private cache: CacheService,
+    private alert: AlertService,
+    private router: Router
+  ) { }
 
+  ngOnInit(): void {
+    return;
+  }
+
+  comecar() {
+    let leitura = this.cache.regrasLidas();
+    if (leitura == null) {
+      this.alert.erro("Leia as regras", "Você não leu as regras né -_-", false, 2500);
+      let i = setInterval(() => {
+        this.router.navigate(['/regras']);
+        clearInterval(i);
+      }, 2500);
+    }
+  }
 }
---
+++ src/app/shared/service/alert.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(
+  ) { }
+  alert(title:any, message:any) {
+    Swal.fire({
+      icon: 'warning',
+      title: title,
+      text: message,
+    })
+  }
+  erro(title:any, message:any, btnConfirm:boolean = false, timer:number = 2000) {
+    Swal.fire({
+      icon: 'error',
+      title: title,
+      text: message,
+      showConfirmButton: btnConfirm,
+      timer: timer
+    })
+  }
+  sussecc(title:any, message:any) {
+    Swal.fire({
+      icon: 'success',
+      title: title,
+      text: message,
+    })
+  }
+}
