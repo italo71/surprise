f813ab76ee6e385f43f474816904ba5a5453f99c
Italo71 <102165012+italo71@users.noreply.github.com>
2023-07-24 17:52:42 -0300

    enigma 3 - inicial

--- src/app/pages/enigmas/e03/e03.component.ts
+++ src/app/pages/enigmas/e03/e03.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/shared/service/alert.service';
+import { CacheService } from 'src/app/shared/service/cache.service';
+import { ComunicationService } from 'src/app/shared/service/comunication.service';
 
 @Component({
   selector: 'app-e03',
   templateUrl: './e03.component.html',
   styleUrls: ['./e03.component.css']
 })
-export class E03Component {
+export class E03Component implements OnInit {
+  soundElement: HTMLAudioElement;
+
+  constructor(
+    private comu: ComunicationService,
+    private alert: AlertService,
+    private cache: CacheService
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.cache.passarNivel(2, null);
+    this.comu.filter('nav-off');
+    this.soundElement = document.getElementById('sound') as HTMLAudioElement;
+    //this.soundElement.play();
+    this.soundElement.volume = (0.05);
+    $('#tela').on('mousemove', (event: any) => {
+      let mousex = event.originalEvent.clientX;
+      let mousey = event.originalEvent.clientY;
+      let caixax = Number(($('#caixa').offset()?.left)?.toFixed(0));
+      let caixay = Number(($('#caixa').offset()?.top)?.toFixed(0));
+      let disx = Math.abs(mousex - caixax);
+      let disy = Math.abs(mousey - caixay);
+      let dis = (disy + disx);
+      let volume = Number((10000 / dis).toFixed(0));
+      this.soundElement.volume = (volume / 100);
+    })
+    //console.log(this.soundElement)
+  }
 
 }
